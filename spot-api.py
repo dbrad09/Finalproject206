5ab2699fa5722e3cf15ea3e452d0a6d01d7644f0
Drew Bradley <DrewBradley@Drews-MacBook-Pro-10.local>
2023-12-06 17:01:16 -0500

    50 song chart working

--- spot-api.py
+++ spot-api.py
@@ -1,26 +1,74 @@
+#import spotipy API to access Spotify content
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-import sys
 
+#othe imports
+import sys
+import sqlite3
+import json
+import os
+import matplotlib.pyplot as plt
+import unittest
+
+#STEP 1: access spotify using credentials below
 
 client_id="bca2c2e8e6d94866aff803ac08baf51f"
 client_secret="c8e5cf89332e4040b2ab70c1b47d9ded"
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-artist_name = []
-track_name = []
-popularity = []
-track_id = []
-for i in range(0,100,50):
-    track_results = sp.search(q='year:2023', type='track', limit=50,offset=i)
-    for i, t in enumerate(track_results['tracks']['items']):
-        artist_name.append(t['artists'][0]['name'])
-        track_name.append(t['name'])
-        track_id.append(t['id'])
-        popularity.append(t['popularity'])
 
-import pandas as pd
-track_dataframe = pd.DataFrame({'artist_name' : artist_name, 'track_name' : track_name, 'track_id' : track_id, 'popularity' : popularity})
-print(track_dataframe.shape)
-track_dataframe.head()
+#STEP 2: Create Database in local environment
+def setUpDatabase(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_name)
+    cur = conn.cursor()
+    return cur, conn
+
+def top_songs():
+    top_100 = sp.playlist_tracks('37i9dQZEVXbMDoHDwVN2tF', limit=50)
+    conn = sqlite3.connect('songs.db')
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS TopSongs (
+                        SongName TEXT,
+                        Artists TEXT,
+                        UNIQUE (SongName, Artists)
+                    )''')
+    conn = sqlite3.connect('songs.db')
+    cursor = conn.cursor()
+    for track in top_100['items']:
+        song_name = track['track']['name']
+        artists = ', '.join([artist['name'] for artist in track['track']['artists']])
+        cursor.execute("INSERT INTO TopSongs (SongName, Artists) VALUES (?, ?)", (song_name, artists))
+    conn.commit()
+    conn.close()
+        
+        
+    
+def artist_appearances(connection, table_name):
+    cursor = connection.cursor()
+
+    # Retrieve data for artist appearances from the database
+    cursor.execute(f"SELECT Artists, COUNT(*) AS Appearances FROM {table_name} GROUP BY Artists")
+    artist_data = cursor.fetchall()
+
+    return artist_data
+
+def plot_artist_comparison(artist, spotify_appearances, apple_appearances):
+    # Plotting comparison between Spotify and Apple Music appearances
+    labels = ['Spotify', 'Apple Music']
+    values = [spotify_appearances.get(artist, 0), apple_appearances.get(artist, 0)]
+
+    plt.bar(labels, values, color=['green', 'blue'])
+    plt.title(f"Artist Appearances: {artist}")
+    plt.xlabel('Platform')
+    plt.ylabel('Number of Appearances')
+    plt.show()
+    
+
+def main():
+    top_songs()
+
+if __name__ == "__main__":
+    main()
+    unittest.main(verbosity=2)
