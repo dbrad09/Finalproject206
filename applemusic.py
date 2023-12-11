76eac304f8b4e3945ece1491e68a0992afc93f51
Drew Bradley <DrewBradley@Drews-MacBook-Pro-10.local>
2023-12-11 15:55:56 -0500

    with visualization

--- applemusic.py
+++ applemusic.py
@@ -1,6 +1,8 @@
 import os
 import requests
 import sqlite3
+import matplotlib.pyplot as plt
+import numpy as np
 
 url = "https://billboard-api5.p.rapidapi.com/api/charts/artist-100"
 
@@ -17,7 +19,7 @@ def create_tables(cursor):
     cursor.execute("CREATE TABLE IF NOT EXISTS second_table (id INTEGER PRIMARY KEY AUTOINCREMENT, rank INTEGER, positionlastweek INTEGER, peakposition INTEGER, weeksonchart INTEGER)")
 
 
-def create_databases():
+def create_database():
     if not os.path.exists('chart_entries.db'):
         conn = sqlite3.connect('chart_entries.db')
         cursor = conn.cursor()
@@ -75,7 +77,51 @@ def delete_database():
         print("Database deleted.")
     else:
         print("Databases do not exist.")
+        
+        
+def calculate_top_avg_weeks():
+    conn_chart = sqlite3.connect('chart_entries.db')
+    conn_second = sqlite3.connect('second_table.db')
 
+    cursor_chart = conn_chart.cursor()
+    cursor_second = conn_second.cursor()
+
+    cursor_chart.execute("""
+        SELECT ce.artist, SUM(st.weeksonchart) AS total_weeks
+        FROM chart_entries ce
+        JOIN second_table st ON ce.rank = st.rank
+        GROUP BY ce.artist
+        ORDER BY total_weeks DESC
+        LIMIT 5;
+    """)
+
+    results = cursor_chart.fetchall()
+
+    conn_chart.close()
+    conn_second.close()
+
+    return results
+
+# Function to visualize top 5 artists with highest average weeks on chart
+def visualize_top_avg_weeks(results):
+    artists = [result[0] for result in results]
+    total_weeks = [result[1] for result in results]
+    colors = plt.cm.viridis(np.linspace(0, 1, len(artists)))
+    
+    plt.figure(figsize=(10, 6))
+    bars = plt.bar(artists, total_weeks, color=colors)
+    plt.xlabel('Artists')
+    plt.ylabel('Total Weeks on Chart')
+    plt.title('Total Weeks on Chart for Top 5 Artists')
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
+    plt.show()
+    
+def write_to_file(results):
+    with open('top_artists_weeks.txt', 'w') as file:
+        file.write("Top 5 Artists and Their Total Weeks on Chart:\n")
+        for result in results:
+            file.write(f"{result[0]}: {result[1]} weeks\n")
 
 def main():
     create_database()
@@ -101,8 +147,11 @@ def main():
    
     conn_second.commit()
     conn_second.close()
+    top_avg_weeks_results = calculate_top_avg_weeks()
+    visualize_top_avg_weeks(top_avg_weeks_results)
+    write_to_file(top_avg_weeks_results)
 
-    delete_database()
+   # delete_database()
     
 
 
