c72d558ae4b125c5faf393b67b67ca09e7ac2cc3
Drew Bradley <DrewBradley@Drews-MacBook-Pro-10.local>
2023-12-12 18:17:56 -0500

    one database for all

--- spot-api.py
+++ spot-api.py
@@ -21,13 +21,18 @@ sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 #STEP 2: Create Database in local environment
 def top_songs():
     top_100 = sp.playlist_tracks('0sDahzOkMWOmLXfTMf2N4N', limit=100)
-    conn = sqlite3.connect('songs.db')
+    conn = sqlite3.connect('chart_entries.db')
     cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Artists (
+                        ArtistID INTEGER PRIMARY KEY,
+                        ArtistName TEXT UNIQUE
+                    )''')
     cursor.execute('''CREATE TABLE IF NOT EXISTS TopSongs (
                         SongName TEXT,
-                        Artists TEXT,
-                        UNIQUE (SongName, Artists)
+                        ArtistIDs TEXT,
+                        FOREIGN KEY (ArtistIDs) REFERENCES Artists(ArtistID)
                     )''')
+
     # Get the count of current entries in the database
     cursor.execute("SELECT COUNT(*) FROM TopSongs")
     current_count = cursor.fetchone()[0]
@@ -39,46 +44,53 @@ def top_songs():
     for i in range(start_index, end_index):
         track = top_100['items'][i]
         song_name = track['track']['name']
-        artists = ', '.join([artist['name'] for artist in track['track']['artists']])
-
-        # Check if the entry already exists
-        cursor.execute("SELECT * FROM TopSongs WHERE SongName = ? AND Artists = ?", (song_name, artists))
-        existing_entry = cursor.fetchone()
-
+        #artists = ', '.join([artist['name'] for artist in track['track']['artists']])
+        artists = [artist['name'] for artist in track['track']['artists']]
+        
+        #store artist IDs for each artist
+        artist_ids= []
+        for artist_name in artists:
+            cursor.execute("SELECT ArtistID FROM Artists WHERE ArtistName= ?", (artist_name,))
+            existing_artist=cursor.fetchone()
+            if existing_artist:
+                artist_id= existing_artist[0]
+            else:
+                cursor.execute("INSERT INTO Artists (ArtistName) VALUES (?)", (artist_name,))
+                artist_id= cursor.lastrowid
+            artist_ids.append(str(artist_id))
+        artist_ids_str= ','.join(artist_ids)
+        cursor.execute("SELECT * FROM TopSongs WHERE SongName = ?", (song_name,))
+        existing_entry= cursor.fetchone()
         if existing_entry is None:
-            cursor.execute("INSERT OR IGNORE INTO TopSongs (SongName, Artists) VALUES (?, ?)", (song_name, artists))
-
+            cursor.execute("INSERT OR IGNORE INTO TopSongs (SongName, ArtistIDs) VALUES (?, ?)", (song_name, artist_ids_str))
     conn.commit()
-    conn.close()
-    # cursor.execute("SELECT COUNT(*) FROM TopSongs")
-    # current_count = cursor.fetchone()[0]
-
-    # for track in top_100['items']:
-    #     song_name = track['track']['name']
-    #     artists = ', '.join([artist['name'] for artist in track['track']['artists']])
+    conn.close()  
 
     #     # Check if the entry already exists
     #     cursor.execute("SELECT * FROM TopSongs WHERE SongName = ? AND Artists = ?", (song_name, artists))
     #     existing_entry = cursor.fetchone()
 
     #     if existing_entry is None:
-    #         cursor.execute("SELECT COUNT (*) FROM TopSongs")
-    #         top_counter= cursor.fetchone()
-    #         if len(top_counter) <= 25:
-    #             cursor.execute("INSERT INTO TopSongs (SongName, Artists) VALUES (?, ?)", (song_name, artists))
+    #         cursor.execute("INSERT OR IGNORE INTO TopSongs (SongName, Artists) VALUES (?, ?)", (song_name, artists))
 
-    # conn.commit()
-    # conn.close()
+    
+    
+    
+    # cursor.execute('''CREATE TABLE IF NOT EXISTS AritstID (
+    #                     Arists TEXT,
+    #                     ID INTEGER,
+    #                     UNIQUE (Artists, ID)
+    #                 )''')
 
 # Retrieve data for artist appearances, and top performing artists from the database
 def artist_appearances():
-    conn = sqlite3.connect('songs.db')
+    conn = sqlite3.connect('chart_entries.db')
     cursor = conn.cursor()
-    cursor.execute("SELECT Artists, COUNT(*) AS Appearances FROM TopSongs GROUP BY Artists")
+    cursor.execute("SELECT ArtistIDs, COUNT(*) AS Appearances FROM TopSongs GROUP BY ArtistIDs")
     artist_data = cursor.fetchall()
     artist_count = {}
     for artists_str in artist_data:
-        artists = artists_str[0].split(', ')
+        artists = artists_str[0].split(',')
         for artist in artists:
             if artist in artist_count:
                 artist_count[artist] += 1
@@ -88,31 +100,61 @@ def artist_appearances():
     conn.close()
     print(artist_count)
     print(top_artists)
+    return artist_count, top_artists
+
+def top_artists_vis(top_artists):
+    conn = sqlite3.connect('chart_entries.db')
+    cursor = conn.cursor()
+    artist_names = {}
+    for artist_id in top_artists.keys():
+        cursor.execute("SELECT ArtistName FROM Artists WHERE ArtistID = ?", (artist_id,))
+        artist_name = cursor.fetchone()
+        if artist_name:
+            artist_names[artist_name[0]] = top_artists[artist_id]
+    conn.close()
+    
     plt.figure(figsize=(10, 6))
-    plt.bar(top_artists.keys(), top_artists.values(), color='skyblue')
+    plt.bar(artist_names.keys(), artist_names.values(), color='skyblue')
     plt.xlabel('Artists')
     plt.ylabel('Number of Appearances')
     plt.title('Top Performing Artists')
     plt.xticks(rotation=45, ha='right')
-    plt.tight_layout()  # Adjusts subplot parameters to give specified padding
+    plt.tight_layout()
     plt.show()
-    return artist_count, top_artists
 
-def write_to_text_file(artist_count, top_artists, file_name):
+
+def text_file(artist_count, top_artists, file_name):
+    conn = sqlite3.connect('chart_entries.db')
+    cursor = conn.cursor()
+    top_artist_names = {}
+    artist_names = {}
+    for artist_id in top_artists.keys():
+        cursor.execute("SELECT ArtistName FROM Artists WHERE ArtistID = ?", (artist_id,))
+        artist_name = cursor.fetchone()
+        if artist_name:
+            top_artist_names[artist_name[0]] = top_artists[artist_id]
+    for artist_id in artist_count.keys():
+        cursor.execute("SELECT ArtistName FROM Artists WHERE ArtistID = ?", (artist_id,))
+        artist_name = cursor.fetchone()
+        if artist_name:
+            artist_names[artist_name[0]] = artist_count[artist_id]
+    conn.close()
+    
     with open(file_name, 'w') as file:
         file.write("Artist Count:\n")
-        for artist, count in artist_count.items():
+        for artist, count in artist_names.items():
             file.write(f"{artist}: {count}\n")
         
         file.write("\nTop Performing Artists:\n")
-        for artist, count in top_artists.items():
+        for artist, count in top_artist_names.items():
             file.write(f"{artist}: {count}\n")
 
 
 def main():
     top_songs()
     artist_count, top_artists = artist_appearances()
-    write_to_text_file(artist_count, top_artists, 'artist_info.txt')
+    top_artists_vis(top_artists)
+    text_file(artist_count, top_artists, 'artist_counts.txt')
     
     
 
